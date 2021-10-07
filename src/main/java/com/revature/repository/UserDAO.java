945b363a6a2f4d9337219ee5f61f856122921f26
ImCoolYeah105 <maw23456789@gmail.com>
2021-10-07 16:46:44 -0400

    add user service

---
+++ src/main/java/com/revature/repository/UserDAO.java
@@ -0,0 +1,13 @@
+package com.revature.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.revature.models.User;
+
+@Repository
+public interface UserDAO extends JpaRepository<User, Integer> {
+	
+	
+	
+}
---
+++ src/main/java/com/revature/service/UserService.java
@@ -0,0 +1,46 @@
+package com.revature.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.revature.models.User;
+import com.revature.repository.UserDAO;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserDAO userDao;
+	
+	@Transactional(readOnly=true)
+	public List<User> findAll() {
+		return this.userDao.findAll();
+	}
+	
+	@Transactional(readOnly=true)
+	public User findById(final int id) {
+		final Optional<User> user = this.userDao.findById(id);
+		return user.isPresent() ? user.get() : null;
+	}
+	
+	@Transactional(propagation=Propagation.REQUIRES_NEW)
+	public User insert(final User user) {
+		return this.userDao.save(user);
+	}
+	
+	@Transactional(propagation=Propagation.REQUIRES_NEW)
+	public void remove(final User user) {
+		this.userDao.delete(user);
+	}
+	
+	@Transactional(propagation=Propagation.REQUIRES_NEW)
+	public void remove(final int id) {
+		this.userDao.deleteById(id);
+	}
+	
+}
