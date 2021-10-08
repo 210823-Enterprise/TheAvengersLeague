8c5cd858429e8707a816acf5a46449ee000ed3d6
ImCoolYeah105 <maw23456789@gmail.com>
2021-10-07 21:04:42 -0400

    add config

--- src/main/java/com/revature/advice/CorsFilter.java
+++ src/main/java/com/revature/advice/CorsFilter.java
@@ -4,16 +4,15 @@ import java.io.IOException;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-@Component
-@WebFilter("/*")
+//@Component
+//@WebFilter("/*")
 public class CorsFilter extends OncePerRequestFilter {
+	
     @Override
     protected void doFilterInternal(final HttpServletRequest req, final HttpServletResponse resp, final FilterChain chain) throws ServletException, IOException {
         resp.setHeader("Access-Control-Allow-Origin", "*");
@@ -22,4 +21,5 @@ public class CorsFilter extends OncePerRequestFilter {
         resp.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
         chain.doFilter(req, resp);
     }
+    
 }
